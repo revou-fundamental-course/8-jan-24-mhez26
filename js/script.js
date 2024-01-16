af74a90cf4f450ffe5a785487e4ccd3114dbfef7
Mhez <indrahutomo26@gmail.com>
2024-01-16 20:40:35 +0700

    minor change

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function luasPersegi(){
+    let panjangSisi = document.getElementById("squareSide").value;
+    let hasilLuas = parseInt(panjangSisi) * parseInt(panjangSisi);
+    document.write("Luas persegi = " + hasilLuas)    
+}
